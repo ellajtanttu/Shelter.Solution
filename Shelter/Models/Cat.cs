52591d50a4de40e48f20cde63aa0f6215e91a768
Ella Tanttu <ellajtanttu@gmail.com>
2022-01-21 12:50:43 -0800

    add dog and cat models

---
+++ Shelter/Models/Cat.cs
@@ -0,0 +1,13 @@
+namespace CretaceousPark.Models
+{
+  public class Dog
+  {
+    public int CatId { get; set; }
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public string Color { get; set; }
+    public bool Intact { get; set; }
+  }
+}
