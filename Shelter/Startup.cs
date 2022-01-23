0d487a955767c04a4e244eaade5466352c7f4ca9
Ella Tanttu <ellajtanttu@gmail.com>
2022-01-23 15:25:12 -0800

    WIP resolving apiversion error

--- Shelter/Startup.cs
+++ Shelter/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Shelter.Models;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 
 namespace Shelter
@@ -25,6 +26,11 @@ namespace Shelter
             services.AddDbContext<ShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddApiVersioning(o => {
+            o.ReportApiVersions = true;
+            o.AssumeDefaultVersionWhenUnspecified = true;
+            o.DefaultApiVersion = new ApiVersion(1, 0);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
